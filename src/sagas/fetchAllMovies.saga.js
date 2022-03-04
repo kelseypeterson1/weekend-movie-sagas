c75f6b40f82da886eb2a44f03dcac950d51fe209
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-03-03 21:35:54 -0600

    finished reorg of reducers and sagas

---
+++ src/sagas/fetchAllMovies.saga.js
@@ -0,0 +1,17 @@
+import { put } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* fetchAllMovies() {
+    // get all movies from the DB
+    try {
+        const movies = yield axios.get('/api/movie');
+        console.log('get all:', movies.data);
+        yield put({ type: 'SET_MOVIES', payload: movies.data });
+
+    } catch {
+        console.log('get all error');
+    }
+        
+}
+
+export default fetchAllMovies;
