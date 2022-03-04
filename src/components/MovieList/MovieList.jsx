e7ec4c853e9bf521462cddb5f0ce1ebf5a5647c4
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-03-03 21:45:03 -0600

    clicking on poster brings user to '/details'

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,16 +1,22 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
-import MovieDetails from '../MovieDetails/MovieDetails'
+import { useHistory } from 'react-router-dom';
 
 function MovieList() {
 
+    const history = useHistory();
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
 
+    //retrieve movies from reducer on page load
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
+
+    const handleClick = () => {
+        history.push('/details');
+    }
 
     return (
         <main>
@@ -20,7 +26,11 @@ function MovieList() {
                     return (
                         <div key={movie.id} >
                             <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
+                            <img 
+                                src={movie.poster} 
+                                alt={movie.title}
+                                onClick={handleClick}
+                            />
                         </div>
                     );
                 })}
