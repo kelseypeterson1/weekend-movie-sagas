b6c24deefccfa479bbd852344090ee68bb313520
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-03-05 17:42:56 -0600

    added use states and onChange effects

--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -5,38 +5,60 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import { useState } from 'react';
 
 export default function MovieForm() {
+
+    const [name, setName] = useState('');
+    const [poster, setPoster] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleSubmit = () => {
+        console.log('submit')
+    }
+
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'center'}}>
-            <Card sx={{ maxWidth: 345 }}>
-                <CardMedia
-                    component="img"
-                    height="140"
-                    image="/static/images/cards/contemplative-reptile.jpg"
-                    alt="green iguana"
-                />
-                <CardContent>
-                    <TextField
-                        id="title"
-                        label="Title"
-                        variant="standard"
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center'
+            }}
+        >
+            <form onSubmit={handleSubmit}>
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image="https://m.media-amazon.com/images/I/71BPuv+iRbL._AC_SL1000_.jpg"
+                        alt="green iguana"
                     />
-                    <br />
-                    <TextField
-                        id="poster"
-                        label="Poster"
-                        variant="standard"
-                    />
-                    <TextField
-                        id="description"
-                        label="Description"
-                        multiline
-                        maxRows={4}
-                        variant="standard"
-                    />
-                </CardContent>
-            </Card>
+                    <CardContent>
+                        <TextField
+                            id="title"
+                            label="Title"
+                            variant="standard"
+                            value={name}
+                            onChange={(event) => setName(event.target.value)}
+                        />
+                        <TextField
+                            id="poster"
+                            label="Poster"
+                            variant="standard"
+                            value={poster}
+                            onChange={(event) => setPoster(event.target.value)}
+                        />
+                        <TextField
+                            id="description"
+                            label="Description"
+                            multiline
+                            maxRows={4}
+                            variant="standard"
+                            value={description}
+                            onChange={(event) => setDescription(event.target.value)}
+                        />
+                    </CardContent>
+                </Card>
+            </form>
         </Box>
     )
 }
