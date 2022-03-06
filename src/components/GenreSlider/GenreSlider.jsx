e88fba95c6bc6f8d2bb52b85fa321014162d3315
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-03-06 13:50:34 -0600

    genre checkboxes no longer functioning

--- src/components/GenreSlider/GenreSlider.jsx
+++ src/components/GenreSlider/GenreSlider.jsx
@@ -1,16 +1,22 @@
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+
 
 export default function GenreSlider({ genre }) {
 
     const dispatch = useDispatch();
+    const [checked, setChecked] = useState(false);
 
     // update selectedGenres reducer
     const handleChange = (event) => {
-        dispatch({ 
+        // setToggle(!toggle);
+        setChecked(event.target.checked);
+
+        dispatch({
             type: 'SELECT_GENRE',
-            payload: [genre.name, true]
+            payload: [genre.name, checked]
         })
     }
 
@@ -18,7 +24,15 @@ export default function GenreSlider({ genre }) {
     return (
         <>
             {/* <FormControlLabel control={<Checkbox onChange={handleChange} defaultChecked />} label={genre.name} /> */}
-            <FormControlLabel control={<Checkbox onChange={handleChange} />} label={genre.name} />
+            <FormControlLabel
+                control=
+                    {<Checkbox
+                        onChange={handleChange}
+                        checked={checked}
+                    />
+                }
+                label={genre.name}
+            />
         </>
     )
 }
