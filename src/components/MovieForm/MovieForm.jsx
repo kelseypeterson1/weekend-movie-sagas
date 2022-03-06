c80536a488df54c3c9e90749d6212b5d58315168
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-03-05 18:28:57 -0600

    added genre sliders

--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -9,6 +9,12 @@ import { useState } from 'react';
 import Button from '@mui/material/Button';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormHelperText from '@mui/material/FormHelperText';
+import Switch from '@mui/material/Switch';
 
 export default function MovieForm() {
 
@@ -19,25 +25,51 @@ export default function MovieForm() {
     const [title, setTitle] = useState('');
     const [poster, setPoster] = useState('');
     const [description, setDescription] = useState('');
+    // genre switches
+    const [state, setState] = React.useState({
+        adventure: false,
+        animated: false,
+        biographical: false,
+        comedy: false,
+        disaster: false,
+        drama: false,
+        epic: false,
+        fantasy: false,
+        musical: false,
+        romantic: false,
+        scienceFiction: false,
+        spaceOpera: false,
+        superhero: false,
+    });
 
+    // submit button posts to DB
     const handleSubmit = (event) => {
         event.preventDefault();
 
         dispatch({
             type: "ADD_MOVIE",
-            payload: 
-                {
-                    title: title,
-                    poster: poster,
-                    description: description
-                }
+            payload:
+            {
+                title: title,
+                poster: poster,
+                description: description,
+                genre_id: 1
+            }
         })
 
         // routes to the movie list page
         history.push('/');
-
-        console.log('submitted!')
     }
+
+
+    // switches change use states
+    const handleChange = (event) => {
+        setState({
+            ...state,
+            [event.target.name]: event.target.checked,
+        });
+    };
+
 
     return (
         <Box
@@ -77,6 +109,95 @@ export default function MovieForm() {
                             value={description}
                             onChange={(event) => setDescription(event.target.value)}
                         />
+
+
+                        {/* Genre selection */}
+                        <FormControl component="fieldset" variant="standard">
+                            <FormGroup>
+                                <FormControlLabel
+                                    control={
+                                        <Switch checked={state.action} onChange={handleChange} name="action" />
+                                    }
+                                    label="Action"
+                                />
+                                <FormControlLabel
+                                    control={
+                                        <Switch checked={state.animated} onChange={handleChange} name="animated" />
+                                    }
+                                    label="Animated"
+                                />
+                                <FormControlLabel
+                                    control={
+                                        <Switch checked={state.biographical} onChange={handleChange} name="biographical" />
+                                    }
+                                    label="Biographical"
+                                />
+                                <FormControlLabel
+                                    control={
+                                        <Switch checked={state.comedy} onChange={handleChange} name="comedy" />
+                                    }
+                                    label="Comedy"
+                                />
+                                <FormControlLabel
+                                    control={
+                                        <Switch checked={state.disaster} onChange={handleChange} name="disaster" />
+                                    }
+                                    label="Disaster"
+                                />
+                                <FormControlLabel
+                                    control={
+                                        <Switch checked={state.drama} onChange={handleChange} name="drama" />
+                                    }
+                                    label="Drama"
+                                />
+                                <FormControlLabel
+                                    control={
+                                        <Switch checked={state.epic} onChange={handleChange} name="epic" />
+                                    }
+                                    label="Epic"
+                                />
+                                <FormControlLabel
+                                    control={
+                                        <Switch checked={state.fantasy} onChange={handleChange} name="fantasy" />
+                                    }
+                                    label="Fantasy"
+                                />
+                                <FormControlLabel
+                                    control={
+                                        <Switch checked={state.musical} onChange={handleChange} name="musical" />
+                                    }
+                                    label="Musical"
+                                />
+                                <FormControlLabel
+                                    control={
+                                        <Switch checked={state.romantic} onChange={handleChange} name="romantic" />
+                                    }
+                                    label="Romantic"
+                                />
+                                <FormControlLabel
+                                    control={
+                                        <Switch checked={state.scienceFiction} onChange={handleChange} name="scienceFiction" />
+                                    }
+                                    label="Science Fiction"
+                                />
+                                <FormControlLabel
+                                    control={
+                                        <Switch checked={state.spaceOpera} onChange={handleChange} name="spaceOpera" />
+                                    }
+                                    label="Space Opera"
+                                />
+                                <FormControlLabel
+                                    control={
+                                        <Switch checked={state.superhero} onChange={handleChange} name="superhero" />
+                                    }
+                                    label="Superhero"
+                                />
+                            </FormGroup>
+                            <FormHelperText>*Must select one</FormHelperText>
+                        </FormControl>
+                        {/* End Genre Selection */}
+
+
                     </CardContent>
                     <CardActions>
                         <Button size="small" type="submit">Submit</Button>
